c5985af50e3b7cbf8253036dcd4fa05ae23b2143
Rudraksh Mahajan <91263379+raiden9420@users.noreply.github.com>
2026-02-20 20:49:47 +0530

    Time: 5 ms (89.36%), Space: 47.8 MB (17.95%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.java
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.java
@@ -1,10 +1,10 @@
 class Solution {
     public int minPartitions(String n) {
-	int max = 0;
-	for (int i=0; i<n.length(); i++) {
-		if (n.charAt(i) - '0' == 9) return 9;
-		max = Math.max(max, (n.charAt(i) - '0'));
-	}
-	return max;
-}
+        int max = 0;
+        for (char ch : n.toCharArray()) {
+            max = Math.max(max, ch - '0');
+        }
+        
+        return max;
+    }
 }
