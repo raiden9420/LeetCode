3f26b6487f52bf8794442899955ec14ec6232c5d
Rudraksh Mahajan <91263379+raiden9420@users.noreply.github.com>
2026-04-03 21:49:49 +0530

    Time: 2 ms (56.23%), Space: 43.2 MB (31.58%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,36 +1,17 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        Stack<Character> stk = new Stack<>();
-        Stack<Character> stk2 = new Stack<>();
+        return build(s).equals(build(t));
+    }
 
-        for(Character ch: s.toCharArray()){
-            if(ch!='#') stk.push(ch);
-            else{
-                if(!stk.empty()) stk.pop();
-               
+    private String build(String str) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : str.toCharArray()) {
+            if (ch != '#') {
+                stack.push(ch);
+            } else if (!stack.isEmpty()) {
+                stack.pop();
             }
         }
-
-        StringBuilder sb = new StringBuilder(stk.size());
-        for (Character ch : stk) {
-            sb.append(ch);
-        }
-
-        for(Character ch: t.toCharArray()){
-            if(ch!='#') stk2.push(ch);
-            else{
-                if(!stk2.empty()) stk2.pop();
-               
-            }
-        }
-
-        StringBuilder sb2 = new StringBuilder(stk2.size());
-        for (Character ch : stk2) {
-            sb2.append(ch);
-        }
-
-        if(sb.toString().equals(sb2.toString()))return true;
-        
-    return false;
+        return String.valueOf(stack);
     }
 }
